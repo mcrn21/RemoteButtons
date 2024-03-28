aab8f2c77a89a6dd625230e970408900ea21584a
Alexander Zhuravlev <av_zhuravlev@bingo-boon.com>
2024-03-28 16:38:33 +0100

    Fix

--- app/src/main/java/com/mcrn21/remotebuttons/Common.java
+++ app/src/main/java/com/mcrn21/remotebuttons/Common.java
@@ -12,15 +12,6 @@ public class Common {
     static String INTENT_ACTION_USB_ATTACHED = "android.hardware.usb.action.USB_DEVICE_ATTACHED";
     static String INTENT_ACTION_USB_DETACHED = "android.hardware.usb.action.USB_DEVICE_DETACHED";
 
-    // Commands
-    static String LEFT_COMMAND = "left";
-    static String RIGHT_COMMAND = "right";
-    static String ENTER_COMMAND = "enter";
-    static String LEFT_UP_COMMAND = "left_up";
-    static String LEFT_DOWN_COMMAND = "left_down";
-    static String RIGHT_UP_COMMAND = "right_up";
-    static String RIGHT_DOWN_COMMAND = "right_down";
-
     // Service
     static String NOTIFICATIONS_CHANNEL_ID = APPLICATION_ID + ".NOTIFICATIONS_CHANNEL";
     static int SERVICE_ID = 1108;
